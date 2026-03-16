47ec5578d8937943317f4fb03c674819ebaf450c
balajivejendla <127621870+balajivejendla@users.noreply.github.com>
2026-03-16 17:23:33 +0530

    commiting all the files

---
+++ backend_server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+const app =express()
+
+app.use(express.json())
+
+
+
+import express from 'express'
+import Apperror from "./apperror.js";
+
+const app=express()
+
+app.use(express.json())
+
+
+
+app.post('/hi',(req,res,next)=>{
+    try{
+        const data=req.body
+
+
+
+        if(typeof(data)!="object"|| data === null|| data.length===0){
+
+
+            return next(new Apperror("Check whether the req body is in right format",400))
+        }
+        else{
+            return res.status(200).json("Good")
+        }
+    }
+    catch(error){
+        next(error)
+    }
+})
+app.use((error,req,res,next)=>{
+    const statuscode= error.statusCode || 500
+    return res.status(statuscode).json({
+        msg:error.message|| "Internal Server Error",
+        stack:error.stack
+    })
+})
+
+app.listen((3000),()=>
+    {
+        console.log("logged")
+    }
+)
